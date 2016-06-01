3e78737e5dd74116b2b94f4395b2832e371b96b3
Megawats777 <jedsemilla@gmail.com>
2016-05-31 21:29:59 -0400

    Added HUD and nav points with Text

---
+++ Assets/Source/HUDController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDController : MonoBehaviour
+{
+    /*--References to HUD Components--*/
+
+    [SerializeField]
+    private Button closeDescriptionButton;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    // Show the description
+    public void showDescription()
+    {
+        // Get the description from the nav point
+
+        // Set the content of the description panel
+
+        // Show the description panel
+
+        // Show the close description panel
+        closeDescriptionButton.gameObject.SetActive(true);
+    }
+
+    // Hide the description
+    public void hideDescription()
+    {
+        // Hide the description Panel
+
+
+        // Hide the close description button
+        closeDescriptionButton.gameObject.SetActive(false);
+    }
+
+    // Exit the App
+    public void exitApp()
+    {
+        Application.Quit();
+        print("App Closed");
+    }
+}
---
+++ Assets/Source/NavPoint.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NavPoint : MonoBehaviour
+{
+    /*--Properties of the class--*/
+
+    // The location of the nav point
+    [SerializeField]
+    private string locationName;
+
+    // Reference to the text of the Nav point
+    [SerializeField]
+    private Text locationText;
+
+    // Is the point selected?
+    private bool isPointSelected = false;
+
+    // Reference to the HUDController
+    private HUDController hudController;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the HUDController
+        hudController = FindObjectOfType<HUDController>();
+
+        // Set the text of the location
+        locationText.text = locationName;
+
+        // Disable the text component
+        locationText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // When this nav point is clicked
+    private void OnMouseDown()
+    {
+        print("Nav Point Selected");
+
+        isPointSelected = true;
+    }
+
+    // When the nav point is hovered show the location text
+    private void OnMouseEnter()
+    {
+        locationText.gameObject.SetActive(true);
+    }
+
+    // When the mouse stops hovering the nav point
+    private void OnMouseExit()
+    {
+        // If the point is not selected then hide the location text
+        if (isPointSelected == false)
+        {
+            locationText.gameObject.SetActive(false);
+        }
+    }
+}
