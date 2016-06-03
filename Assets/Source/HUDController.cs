95c707d60559e8c735feff9d5bddf50caa39b332
Megawats777 <jedsemilla@gmail.com>
2016-06-03 16:44:16 -0400

    Added animation to the description panel

--- Assets/Source/HUDController.cs
+++ Assets/Source/HUDController.cs
@@ -15,6 +15,9 @@ public class HUDController : MonoBehaviour
     [SerializeField]
     private Text descriptionText;
 
+    [SerializeField]
+    private Text locationTitle;
+
     /*--Properties of the class--*/
 
     // Is a nav point selected
@@ -25,18 +28,80 @@ public class HUDController : MonoBehaviour
     //[HideInInspector]
     public NavPoint selectedNavPoint;
 
+    /*--Description Panel Animation Properties--*/
+
+    // The Description panel position lerp value
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float panelPositionLerpValue = 0.0f;
+
+    // The starting Y position for the description panel
+    private float panelStartYPosition;
+
+    // The starting X position for the description panel
+    private float panelStartXPosition;
+
+    // The ending X position for the description panel
+    [SerializeField]
+    private float panelEndXPosition;
+
+    // The new X position for the description panel
+    private float panelNewXPosition;
+
+    // Panel animation rate
+    [SerializeField, Range(0.0f, 250)]
+    private float panelAnimationRate = 1.0f;
+
+    // Is the description panel opening
+    public bool isPanelClosing = false;
+
     // Use this for initialization
     void Start ()
     {
         // Hide the close description button
         closeDescriptionButton.gameObject.SetActive(false);
+
+        // Set the start X position for the description panel
+        panelStartXPosition = descriptionPanel.transform.position.x;
+
+        // Set the start Y position for the description panel
+        panelStartYPosition = descriptionPanel.transform.position.y;
+
+        // Set the content of the description text and location title to null
+        locationTitle.text = null;
+        descriptionText.text = null;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        // Control the animation of the description panel
+        controlPanelAnimation();
+
 	}
+
+    // Control the animation of the description panel
+    private void controlPanelAnimation()
+    {
+        // If the descriptionPanel is closing decrease the position lerp value
+        if (isPanelClosing == true)
+        {
+            // Decrease the panel position lerp value
+            panelPositionLerpValue = Mathf.Clamp(panelPositionLerpValue - 0.01f * panelAnimationRate * Time.deltaTime, 0.0f, 1.0f);
+        }
+
+        // If the descriptionPanel is not closing increase the position lerp value
+        if (isPanelClosing == false)
+        {
+            // Increase the panel position lerp value
+            panelPositionLerpValue = Mathf.Clamp(panelPositionLerpValue + 0.01f * panelAnimationRate * Time.deltaTime, 0.0f, 1.0f);
+        }
+
+        // Set the new position variable the new position
+        panelNewXPosition = Mathf.Lerp(panelStartXPosition, panelEndXPosition, panelPositionLerpValue);
+
+        // Set the new X position of the descriptionPanel
+        descriptionPanel.transform.position = new Vector3(panelNewXPosition, panelStartYPosition, 0.0f);
+    }
 
     // Show the description
     public void showDescription(GameObject navPointObject)
@@ -49,8 +114,11 @@ public class HUDController : MonoBehaviour
         // Set the content of the description panel
         descriptionText.text = selectedNavPoint.locationDescription;
 
-        // Show the description panel
+        // Set the content of the location title
+        locationTitle.text = selectedNavPoint.locationName;
 
+        // Show the description panel
+        isPanelClosing = false;
 
         // Show the close description panel
         closeDescriptionButton.gameObject.SetActive(true);
@@ -64,14 +132,11 @@ public class HUDController : MonoBehaviour
         // Hide the selected nav point location text
         selectedNavPoint.locationText.gameObject.SetActive(false);
 
-        // Clear the current description text content
-        descriptionText.text = null;
-
         // Remove the nav point reference
         selectedNavPoint = null;
 
         // Hide the description Panel
-
+        isPanelClosing = true;
 
         // Hide the close description button
         closeDescriptionButton.gameObject.SetActive(false);
